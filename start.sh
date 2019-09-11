fb5445dd91763307d2b9a4f803e7a0292d537e3f
ivanajovicic <ivanajovicic85@gmail.com>
2019-09-11 16:10:48 -0700

    files updated

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 sudo rm -rf __pycache__/ dask-worker-space/ .ipynb_checkpoints/ storage  lts_data/
 
-docker run --runtime=nvidia -v /home/ijovicic/rapids_repos/:/rapids/notebooks/ -p 8888:8888 -p 8787:8787 ml_tutorial:v2
+docker run --runtime=nvidia -v /home/ijovicic/rapids_repos/:/rapids/notebooks/ -p 8888:8888 -p 8787:8787 ml_tutorial
 
 
